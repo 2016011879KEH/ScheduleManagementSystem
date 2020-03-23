a09d15fee541e79d8cd2cd346b56d2a631f96525
com119 <com119@125.134.74.99>
2020-03-23 18:54:49 +0900

    create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("***Schedule Management System Menu***");
+		System.out.println(" 1.Add Schedule");
+		System.out.println(" 2.Delete Schedule");
+		System.out.println(" 3.Edit Schedule");
+		System.out.println(" 4.View Schedule");
+		System.out.println(" 5.Exit");
+		System.out.println("Select one number between 1-5:");
+	}
+
+}
